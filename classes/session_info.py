19fd016c5350c8a9a51ecfb10410a045a6383aa9
Kapil Gaire <eriaglipak312@gmail.com>
2026-08-14 21:25:09 +0545

    refactor: set market_id and client id

--- classes/session_info.py
+++ classes/session_info.py
@@ -1,3 +1,14 @@
+from exceptions import (
+    AccessTokenFetchError,
+    AccessTokenInvalidError,
+    InvalidServerResponseError,
+)
+from fetch_handlers import get_access_and_refresh_token, get_market_open_info
+from main import access_tokens, market_id
+from modify_access_token import modify_access_token
+from utils import calculate_client_id
+
+
 class SessionInfo:
     def __init__(self) -> None:
         # token to be sent to client to verify identity
@@ -7,7 +18,7 @@ class SessionInfo:
 
         # salt values sent by server in fetch_token_endpint,
         # are used to create a id which needs to be sent as payload to server when requesting data, stays same for one session.
-        self.access_tokens: list = []
+        self.access_tokens: list[int] = []
 
         # id to be sent to server when requesting the data
         self.client_id: int | None = None
@@ -15,3 +26,57 @@ class SessionInfo:
         # id returned byr market_open_endpoint,
         # used to calculate the client_id
         self.market_id: int | None = None
+
+    async def set_access_and_refresh_token(self) -> None:
+        try:
+            data = await get_access_and_refresh_token()
+
+            self.refresh_token = data.get("refreshToken")
+
+            self.access_tokens = [
+                data.get("salt1"),
+                data.get("salt2"),
+                data.get("salt3"),
+                data.get("salt4"),
+                data.get("salt5"),
+            ]
+
+            # update the access token sent by server with the wasm file
+            original_access_token = data.get("accessToken")
+            updated_access_token = modify_access_token(
+                original_access_token=original_access_token,
+                salt_values=self.access_tokens,
+            )
+            self.access_token = updated_access_token
+
+        except AccessTokenFetchError as e:
+            print("Failed setting the access token.")
+            print("Error occured:", e.__cause__)
+
+        except InvalidServerResponseError as e:
+            print(e)
+
+        except Exception as e:
+            print(e)
+
+    async def set_market_id(self):
+        try:
+            data = await get_market_open_info(self.access_token)
+            self.market_id = data.get("id")
+
+        except AccessTokenInvalidError as e:
+            print(e)
+
+        except InvalidServerResponseError as e:
+            print(e)
+
+        except Exception as e:
+            print(e)
+
+    def set_client_id(self):
+        if (self.market_id is None) or (not self.access_tokens):
+            print("Market id and/or access_tokens not set.")
+            return
+        self.client_id = calculate_client_id(
+            market_id=self.market_id, access_tokens=self.access_tokens
+        )
--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from datetime import date
+
+from dummy_data_arr import dummyData
+
+
 def get_full_headers(default_headers, access_token):
     # construct the authorization header
     auth_header = {
@@ -7,3 +12,17 @@ def get_full_headers(default_headers, access_token):
     # full header
     full_header = default_headers | auth_header
     return full_header
+
+
+def calculate_client_id(market_id: int, access_tokens: list[int]) -> int:
+    # get todays day int value
+    day = date.today().day
+    dummyId = market_id
+
+    # calculating id based on nepalstock website
+    l = dummyData[dummyId] + dummyId + 2 * day
+
+    selector: int = 1 if (l % 10 < 5) else 3
+
+    id = l + access_tokens[selector] * day - access_tokens[selector - 1]
+    return id
